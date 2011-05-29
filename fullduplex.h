1ce696df2e8ce92291706e5bf161a88831f162c2
Philippe Mercure <philippemercure@mac.com>
2011-05-29 15:26:41 -0400

    Change client et server

--- fullduplex.h
+++ fullduplex.h
@@ -1,9 +1,7 @@
-#define np_client_server	"/tmp/SIRA_np_client_server"
-#define np_server_client	"/tmp/SIRA_np_server_client"
+#define np_client_server	"/tmp/MERP_np_client_server"
+#define np_server_client	"/tmp/MERP_np_server_client"
 #define MAX_BUF_SIZE		255
 
-static const char * rep_travail 		= "/tmp/SIRA/";
-static const char * rep_client  		= "/tmp/SIRA/client/";
-static const char * rep_client_fichiers		= "/tmp/SIRA/client/fichiers/";
-static const char * rep_client_resultats	= "/tmp/SIRA/client/resultats/";
-static const char * rep_server  	 	= "/tmp/SIRA/server/";
+static const char * rep_travail 		= "/tmp/MERP/";
+static const char * rep_client  		= "/tmp/MERP/client/";
+static const char * rep_server  	 	= "/tmp/MERP/server/";
