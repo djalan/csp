cddaf493fc5544c4d6c5f48cd4340be1b94b3289
Alain Sirois <alainsirois@gmail.com>
2011-05-29 20:01:16 -0400

    unstage: fullduplex.h

--- fullduplex.h
+++
@@ -1,7 +0,0 @@
-#define np_client_server	"/tmp/SIRA_np_client_server"
-#define np_server_client	"/tmp/SIRA_np_server_client"
-#define MAX_BUF_SIZE		255
-
-static const char * rep_travail 		= "/tmp/SIRA/";
-static const char * rep_client  		= "/tmp/SIRA/client/";
-static const char * rep_server  	 	= "/tmp/SIRA/server/";
